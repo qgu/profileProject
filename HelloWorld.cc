67afbd3554c1815bb2217a1a59e9e9b293e00224
qgu <qiushi.gu@diamond.ac.uk>
2015-07-01 12:00:53 +0100

    preparing to merge

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -17,12 +17,16 @@ int main(){
 	int times = 100000;
 
 	vector<double> u,v,w,y;
-	//I am in branch1
+	//this is used to change the file
 	for(int i= 0; i < times; i++) u.push_back(sin(i));
 	for(int i= 0; i < times; i++) v.push_back(square(i));
 	for(int i= 0; i < times; i++) w.push_back(i*i);
 	for(int i= 0; i < times; i++) y.push_back(factorial(i));
+<<<<<<< .merge_file_9z5DJ5
 	//for(int i= 0; i < times; i++) z.push_back(square(factorial(i)));
+=======
+	//this is supposed to be in the add_function branch
+>>>>>>> .merge_file_LEnZ63
 	cout << "Hello World" << endl;
 	cout << times << endl;
 	return 0;

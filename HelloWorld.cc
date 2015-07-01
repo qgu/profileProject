1af99386b82a0f0cadf80442c80a1fb51918dc17
qgu <qiushi.gu@diamond.ac.uk>
2015-07-01 11:37:48 +0100

    Added some comment

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -17,7 +17,7 @@ int main(){
 	int times = 100000;
 
 	vector<double> u,v,w,y;
-
+	//this is used to change the file
 	for(int i= 0; i < times; i++) u.push_back(sin(i));
 	for(int i= 0; i < times; i++) v.push_back(square(i));
 	for(int i= 0; i < times; i++) w.push_back(i*i);
